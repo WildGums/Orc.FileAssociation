2b7d2099d3c4a6160d4a55f16acf68acbb0b4c39
Geert van Horrik <geert@catenalogic.com>
2016-02-05 18:12:31 +0100

    Solution maintenance

--- src/Orc.FileAssociation/Orc.FileAssociation.Shared/Services/FileAssociationService.cs
+++ src/Orc.FileAssociation/Orc.FileAssociation.Shared/Services/FileAssociationService.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="FileAssociationService.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="FileAssociationService.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
