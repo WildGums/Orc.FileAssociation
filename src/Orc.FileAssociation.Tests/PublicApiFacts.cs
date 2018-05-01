79d5dea53db2d42b63d1362b68320f9e5b8d9ab4
Geert van Horrik <geert@catenalogic.com>
2018-05-01 10:12:27 +0200

    Migrate test project to new project structure

--- src/Orc.FileAssociation.Tests/PublicApiFacts.cs
+++ src/Orc.FileAssociation.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.FileAssociation.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_FileAssociation_HasNoBreakingChanges()
         {
             var assembly = typeof(FileAssociationService).Assembly;
