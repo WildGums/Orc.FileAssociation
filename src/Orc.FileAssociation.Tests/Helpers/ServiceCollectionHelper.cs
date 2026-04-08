8fe18b62d5ae53fe46632d48aaa8aff3538aecd9
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:26:22 +0200

    Convert to file scoped namespaces

--- src/Orc.FileAssociation.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.FileAssociation.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,21 +1,20 @@
-﻿namespace Orc.FileAssociation.Tests
+﻿namespace Orc.FileAssociation.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+using Orc.FileSystem;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-    using Orc.FileSystem;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcFileSystem();
-            serviceCollection.AddOrcFileAssociation();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcFileSystem();
+        serviceCollection.AddOrcFileAssociation();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
--- src/Orc.FileAssociation/OrcFileAssociationModule.cs
+++ src/Orc.FileAssociation/OrcFileAssociationModule.cs
@@ -1,26 +1,25 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.FileAssociation;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcFileAssociationModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.FileAssociation;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcFileAssociationModule
+    public static IServiceCollection AddOrcFileAssociation(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcFileAssociation(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<IApplicationRegistrationService, ApplicationRegistrationService>();
-            serviceCollection.TryAddSingleton<IFileAssociationService, FileAssociationService>();
+        serviceCollection.TryAddSingleton<IApplicationRegistrationService, ApplicationRegistrationService>();
+        serviceCollection.TryAddSingleton<IFileAssociationService, FileAssociationService>();
 
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.FileAssociation", "Orc.FileAssociation.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.FileAssociation", "Orc.FileAssociation.Properties", "Resources"));
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.FileAssociation", "https://github.com/wildgums/orc.fileassociation"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.FileAssociation", "https://github.com/wildgums/orc.fileassociation"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
